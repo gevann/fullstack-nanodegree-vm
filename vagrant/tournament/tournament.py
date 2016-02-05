325ac943ea60ce885a885857dac565c47a4b8c7c
Graeme <graemenathan@gmail.com>
2016-02-04 20:22:55 -0800

    feat: Add create table statements

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,29 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    pass
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    pass
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    pass
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    pass
 
 
 def playerStandings():
@@ -47,6 +51,7 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    pass
 
 
 def reportMatch(winner, loser):
@@ -56,16 +61,17 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    pass
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +79,4 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    pass
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,30 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS Players CASCADE;
+CREATE TABLE Players(
+    id INT,
+    firstName VARCHAR(15),
+    lastName VARCHAR(50),
+    rank INT,
+    PRIMARY KEY(id));
 
+DROP TABLE IF EXISTS Matches CASCADE;
+CREATE TABLE Matches(
+    id INT,
+    winner INT,
+    loser INT,
+    round INT,
+    FOREIGN KEY(winner) REFERENCES Players(id),
+    FOREIGN KEY(loser) REFERENCES Players(id),
+    PRIMARY KEY(id));
+
+DROP TABLE IF EXISTS PlaysIn CASCADE;
+CREATE TABLE PlaysIn(
+    player_1_id INT,
+    player_2_id INT,
+    match_id INT,
+    FOREIGN KEY(player_1_id) REFERENCES Players(id),
+    FOREIGN KEY(player_2_id) REFERENCES Players(id),
+    FOREIGN KEY(match_id) REFERENCES Matches(id),
+    PRIMARY KEY(match_id));

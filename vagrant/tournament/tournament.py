141637d7ab163c129d6e5ed56dcbc3eaa32c8f07
Graeme <graemenathan@gmail.com>
2016-02-07 18:40:34 -0800

    style: Remove Cursor class def from tournament.py

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -2,28 +2,7 @@
 #
 # tournament.py -- implementation of a Swiss-system tournament
 #
-
-import psycopg2
-
-
-class Cursor():
-    def __init__(self):
-        pass
-
-    def __enter__(self):
-        self.conn = psycopg2.connect("dbname=tournament")
-        self.cursor = self.conn.cursor()
-        return self.cursor
-
-    def __exit__(self, exec_type, exec_val, exec_tb):
-        self.conn.commit()
-        self.cursor.close()
-        self.conn.close()
-
-
-def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+from Cursor import Cursor
 
 
 def deleteMatches():
